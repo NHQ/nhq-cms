0dd16cb3ad5976462a7c221cb9e82a9b3573cd15
Marc Stevens <marcstevens@gmail.com>
2010-09-28 12:42:50 -0500

    initialized haml with xhtml, updated Show index

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Haml::Template.options[:format] = :html5
-
 # Initialize the rails application
 Clown::Application.initialize!
