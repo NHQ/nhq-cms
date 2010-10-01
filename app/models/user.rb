e1f535dca8b6f9204bbc7da6ccaed389d7439792
Marc Stevens <marcstevens@gmail.com>
2010-10-01 12:28:03 -0500

    disabled user registration, tweaked application layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable, :registerable, and :timeoutable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   field :name
