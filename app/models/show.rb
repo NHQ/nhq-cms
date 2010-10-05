5c992c57c31f3e815d39a7e48894aedb693c957f
Marc Stevens <marcstevens@gmail.com>
2010-10-05 06:14:37 -0500

    removed pre-mature credit code from show model

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,9 +7,6 @@ class Show
   field :description
   field :additional_info
 
-  embeds_many :credits
-  accepts_nested_attributes_for :credits, :reject_if => lambda { |t| t['role'].nil? }, :allow_destroy => true
-
   mount_uploader :image, ImageUploader
 
   references_many :reviews
